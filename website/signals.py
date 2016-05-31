a643d7c9473e69a82713b41dd1f5b0e983f9c75f
Jon Froehlich <jonfroehlich@gmail.com>
2016-05-31 14:42:38 -0400

    Added auto-thumbnail generation for Talks
    Added auto-thumbnail generation for Talks

--- website/signals.py
+++ website/signals.py
@@ -6,69 +6,48 @@ from django.conf import settings
 import os
 from django.core.files import File
 
+#
+# A note about the @receiver decorator used in signals.py.
+# There are two ways to hook up receiver functions
+# 1. You can hook them up manually:
+#     from django.core.signals import request_finished
+#     request_finished.connect(my_callback)
+#
+# 2. You can use a receiver() decorator (the @receiver thing). This is the approach we are using.
+#    In order for this to work, we must add in an import website.signals to the ready() function
+#    in apps.py, which we've done.
+# See : https://docs.djangoproject.com/en/1.9/topics/signals/#receiver-functions
+#
+
+# Called automatically by Django after Publication is saved using Django's
+# built-in signal dispatch functionality. We use this function to do some
+# post-processong on the uploaded Publication data like auto-generating a thumbnail
+# For more info on Django signal dispatch, see: https://docs.djangoproject.com/en/1.9/topics/signals/
 @receiver(post_save, sender=Publication)
-def save_publication(sender, **kwargs):
+def publication_post_save(sender, **kwargs):
     # See: http://www.yaconiello.com/blog/auto-generating-pdf-covers/
     # http://stackoverflow.com/questions/1308386/programmatically-saving-image-to-django-imagefield
 
-    # get the publication that was just saved
+    # get the publication that was just saved and auto-generate a thumbnail
     pub = kwargs['instance']
     print("Publication '{}' has just been saved with PDF={}, checking to see if we should auto-generate a thumbnail".format(pub.title, pub.pdf_file.path))
     thumbnail_res = 300
     generate_and_save_thumbnail_from_pdf(pub, thumbnail_res)
 
-post_save.connect(save_publication, sender=Publication)
-
+# Called automatically by Django after Talk is saved using Django's
+# built-in signal dispatch functionality. We use this function to do some
+# post-processong on the uploaded Talk data like auto-generating a thumbnail
+# For more info on Django signal dispatch, see: https://docs.djangoproject.com/en/1.9/topics/signals/
 @receiver(post_save, sender=Talk)
-def my_handler(sender, **kwargs):
+def talk_post_save(sender, **kwargs):
     # See: http://www.yaconiello.com/blog/auto-generating-pdf-covers/
     # http://stackoverflow.com/questions/1308386/programmatically-saving-image-to-django-imagefield
 
-    # # get the talk that was just saved
+    # get the talk that was just saved and auto-generate a thumbnail
     talk = kwargs['instance']
     print("Talk '{}' has just been saved with PDF={}, checking to see if we should auto-generate a thumbnail".format(talk.title, talk.pdf_file.path))
     thumbnail_res = 300
     generate_and_save_thumbnail_from_pdf(talk, thumbnail_res)
-
-    #
-    # # thumbnailPath = os.path.join(talk.thumbnail.storage.base_location, talk.thumbnail.field.upload_to)
-    # # thumbnail_dir = os.path.dirname(talk.pdf_file.path)
-    # thumbnail_dir = os.path.normpath(os.path.normcase(os.path.join(settings.MEDIA_ROOT, talk.thumbnail.field.upload_to)))
-    #
-    # # make sure this dir exists
-    # if not os.path.exists(thumbnail_dir):
-    #     os.makedirs(thumbnail_dir)
-    #
-    # pdf_filename = os.path.basename(talk.pdf_file.path)
-    # pdf_filename_no_extension = os.path.splitext(pdf_filename)[0]
-    # thumbnail_filename = "{}.{}".format(pdf_filename_no_extension, "jpg");
-    # thumbnail_path = os.path.join(thumbnail_dir, thumbnail_filename)
-    #
-    # # check to see if this is a new (or changed) file. This 'if condition' is super necessary
-    # # because otherwise we would enter an infinite loop given that we save the model again below
-    # if talk.thumbnail.name is None or \
-    #                 os.path.normpath(os.path.normcase(talk.thumbnail.path)) != os.path.normpath(os.path.normcase(thumbnail_path)):
-    #     with Image(filename="{}[0]".format(talk.pdf_file.path), resolution=thumbnail_res) as img:
-    #          img.save(filename=thumbnail_path)
-    #
-    #     # talk.thumbnail = thumbnail_path
-    #     testpath = os.path.join(talk.thumbnail.field.upload_to, thumbnail_filename)
-    #     talk.thumbnail = testpath
-    #     # talk.thumbnail.save(thumbnail_dir, File(open(thumbnail_path)))
-    #     talk.save()
-    # else:
-    #     print("No need to save!")
-
-@receiver(post_save, sender=Poster)
-def my_handler(sender, **kwargs):
-    # See: http://www.yaconiello.com/blog/auto-generating-pdf-covers/
-    # http://stackoverflow.com/questions/1308386/programmatically-saving-image-to-django-imagefield
-
-    # get the poster that was just saved
-    poster = kwargs['instance']
-    print("Poster '{}' has just been saved with PDF={}, checking to see if we should auto-generate a thumbnail".format(poster.title, poster.pdf_file.path))
-    thumbnail_res = 300
-    generate_and_save_thumbnail_from_pdf(poster, thumbnail_res)
 
 # Assumes that artifact is a models.Model type and has the following fields:
 #  an ImageField called thumbnail
