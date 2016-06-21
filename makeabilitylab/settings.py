ab6abc245d86b175bf770e12596e8e51cf92d7d6
Jamie Gilkeson <jamgilk@makeabilitylab.umiacs.umd.edu>
2016-06-21 14:42:57 -0400

    Updated the settings file to move sqlite to a db directory to make gunicorn work in production

--- makeabilitylab/settings.py
+++ makeabilitylab/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'makeabilitylab.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db/db.sqlite3'),
     }
 }
 
