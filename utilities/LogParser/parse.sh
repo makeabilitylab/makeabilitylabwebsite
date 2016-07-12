c36c076a127406cf1eea854b52036faa8020932e
Jamie Gilkeson <rangedenemy@gmail.com>
2016-07-12 11:24:37 -0400

    Fixed an issue with the parser in instances where the log has been overwritten

--- utilities/LogParser/parse.sh
+++ utilities/LogParser/parse.sh
@@ -4,10 +4,6 @@ then
 else
     file=$1
 fi
-    
-if [ -f output ]
-then
-    grep pdf $file | sed -n -e "/$(tail -n 1 output | awk '{print $1}' | cut -c 2- | awk 'BEGIN{FS="/"} {print $3}')/,\$p" | tail -n +2 | awk '{print $4  $7}' | awk 'BEGIN{FS="/"} {print $1 "/" $2 "/" $3 "_" $5 "_" $6}' | awk 'BEGIN{FS="_"} {$NF=""; print $0}'  >> output
-else
-    grep pdf $1 | awk '{print $4  $7}' | awk 'BEGIN{FS="/"} {print $1 "/" $2 "/" $3 "_" $5 "_" $6}' | awk 'BEGIN{FS="_"} {$NF=""; print $0}'  >> output
-fi
+grep pdf $1 | awk '{print $4  $7}' | awk 'BEGIN{FS="/"} {print $1 "/" $2 "/" $3 "_" $5 "_" $6}' | awk 'BEGIN{FS="_"} {$NF=""; print $0}'  >> output
+output | sort | uniq > output
+
