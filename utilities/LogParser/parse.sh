010e110e8c4570229b7d99e586db9f21c4f4728e
Jamie Gilkeson <rangedenemy@gmail.com>
2016-07-12 11:26:20 -0400

    Fixed last issue with parser

--- utilities/LogParser/parse.sh
+++ utilities/LogParser/parse.sh
@@ -5,5 +5,5 @@ else
     file=$1
 fi
 grep pdf $file | awk '{print $4  $7}' | awk 'BEGIN{FS="/"} {print $1 "/" $2 "/" $3 "_" $5 "_" $6}' | awk 'BEGIN{FS="_"} {$NF=""; print $0}'  >> output
-output | sort | uniq > output
+sort output | uniq > output
 
