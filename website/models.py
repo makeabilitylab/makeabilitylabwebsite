18b1647aba586e124647049a79d7d15af2430860
Jamie Gilkeson <rangedenemy@gmail.com>
2016-08-29 09:22:55 -0400

    Updated help text for project header

--- website/models.py
+++ website/models.py
@@ -307,9 +307,10 @@ class Project_Role(models.Model):
 
 #This class contains the iamge or video which will appear in the top description of each project. It functions as a combination of Photo and Video, but is seperated to make it simpler to have a specific video or photo as the projects header.
 class Project_header(models.Model):
-    video_url = models.URLField(blank=True, null=True)
     title = models.CharField(max_length=255)
+    title.help_text="These fields are used as the image in the about section. To add a banner to your page go to the banners table and assign banners to your project using the project field there. This field will accept both a video and an image. If both are provided the video should be prefered."
     caption = models.CharField(max_length=255, blank=True, null=True)
+    video_url = models.URLField(blank=True, null=True)
     image = models.ImageField(upload_to='projects/images/', blank=True, null=True, max_length=255)
     project = models.ForeignKey(Project, blank=True, null=True)
 
