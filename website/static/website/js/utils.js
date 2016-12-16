73f344fd194267564e356be170efa407a98955c6
lstearns86 <lstearns86@gmail.com>
2016-12-16 13:50:21 -0500

    Fixing recursion issue with height fix

--- website/static/website/js/utils.js
+++ website/static/website/js/utils.js
@@ -1,6 +1,7 @@
 function heightFix(className) {
 	var maxActive=0;
     $(className).each(function(index){
+    	$(this).height('auto');
 		if (this.scrollHeight>maxActive)
 		    maxActive = this.scrollHeight;
 	    });
