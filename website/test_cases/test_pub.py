a978b22c8388f66538ee42338d119e8079e6d78d
unknown <Administrator@sbhatt-mobile.redmond.corp.microsoft.com>
2018-06-26 15:26:56 -0700

    made test data folder, put first test case for publications

---
+++ website/test_cases/test_pub.py
@@ -0,0 +1,11 @@
+from website.models import Publication
+from django.test import TestCase
+
+
+class PublicationsTest(TestCase):
+    def test_pubs_exist(self):
+        self.assertNotEqual(Publication.objects.all().count(), 0, 'no publications!')
+        for pub in Publication.objects.all():
+            self.assertNotEqual(pub.pdf_file, None, 'pdf for publication ' + pub.title + ' is missing!')
+
+    
