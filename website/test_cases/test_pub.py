d85257cf3315d93400028d27c92638af5f824a44
Shiven Bhatt <shivenbhatt@outlook.com>
2018-06-27 11:08:26 -0700

    updated publication and video tests

--- website/test_cases/test_pub.py
+++ website/test_cases/test_pub.py
@@ -1,21 +1,28 @@
 from website.models import Publication
 from django.test import TestCase
-import requests
+from website.test_cases.helper_functions import *
 
 
 class PublicationsTest(TestCase):
     def test_pubs_exist(self):
         self.assertNotEqual(Publication.objects.all().count(), 0, 'no publications!')
+
+    def test_pubs_pdf_exist(self):
         for pub in Publication.objects.all():
-            self.assertNotEqual(pub.pdf_file, None, 'pdf for publication ' + pub.title + ' is missing!')
+            self.assertNotEqual(pub.pdf_file, None, 'pdf for publication ' + pub.title + ' does not exist!')
+
+    def test_pubs_video_exist(self):
+        for pub in Publication.objects.all():
+            self.assertEqual(check_site_exist(pub.video.video_url), True, 'video url for publication ' + pub.title + ' returns error')
+
+    def test_pubs_thumbnail_exist(self):
+        for pub in Publication.objects.all():
+            self.assertNotEqual(pub.thumbnail, None, 'thumbnail for publication ' + pub.title + ' does not exist!')
 
 
-def check_site_exist(url):
-    try:
-        site_ping = requests.head(url)
-        if site_ping.status_code < 400:
-            return True
-        else:
-            return False
-    except Exception:
-        return False
+
+
+
+
+
+
