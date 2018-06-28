6f2a41e7004d827d6655d661dc30f09ae1cc41ca
Johnson Kuang <johnsonkuang@outlook.com>
2018-06-28 11:04:53 -0700

    Testing on non-Docker env

--- website/test_cases/test_images.py
+++ website/test_cases/test_images.py
@@ -6,22 +6,29 @@ from django.core.files import File
 import website.test_cases.helper_functions as hf
 
 
-
 class ImageModelTest(TestCase):
     def setUp(self):
         '''
-        a = hf.get_files_in_dir_in_testData('.png', 'testJPEGs')
-        print(a)
-        for img_path in a:
-            print(img_path)
-            image = File(open(img_path, 'rb'))
-            new_photo = Photo.objects.create(picture=image, caption="hello", alt_text="hello")
+        with open('./media/testData/testJPGs/jpeg_urls.txt', 'w') as fwrite_urls:
+            a = hf.get_files_in_dir_in_testData('.jpg', 'testData/testJPGs')
+            for item in a:
+                fwrite_urls.write("%s\n" % item)
+        '''
+        with open('./media/testData/testJPGs/jpeg_urls.txt', 'r') as fin:
+            raw_lines = fin.readlines()
+        lines = list(map(lambda s: s.strip(), raw_lines))
+        print(lines)
+        for i in range(len(lines)):
+            file_name = "test"+str(int(i+1))+".jpg"
+            print(file_name)
+            image = File(open("./media/testData/testJPGs/"+file_name, 'rb'))
+            new_photo = Photo(picture=image, caption="test", alt_text="TEST")
             new_photo.save()
         '''
-        image = File(open("./media/testData/testJPEGs/test1.jpeg", 'rb'))
+        image = File(open("./media/testData/testJPGs/test1.jpg", 'rb'))
         new_photo = Photo(picture=image, caption="test", alt_text="TEST")
         new_photo.save()
-
+        '''
     def test_images_not_empty(self):
         self.assertNotEqual(Photo.objects.all().count(), 0, 'no photos!')
 
