bf8895dc9fbab7a085b671644093ded6b5dc6772
Johnson Kuang <johnsonkuang@outlook.com>
2018-06-28 12:41:08 -0700

    fixed image issues

--- website/test_cases/test_images.py
+++ website/test_cases/test_images.py
@@ -2,32 +2,37 @@ from django.test import TestCase
 from website.models import Photo
 from django.core.files.images import ImageFile
 from django.core.files import File
+from PIL import Image
 
 import website.test_cases.helper_functions as hf
 
 
 class ImageModelTest(TestCase):
     def setUp(self):
+        operationGetThoseURLS = hf.get_files_in_dir_in_testData('.jpeg', 'testData/testJPGs')
+        for urls in operationGetThoseURLS:
+            image = File(open(urls, 'rb'))
+            print(urls)
+            new_photo = Photo(picture=image, caption="test", alt_text="TEST")
+            print(new_photo)
+            new_photo.save()
+            image.close()
         '''
-        with open('./media/testData/testJPGs/jpeg_urls.txt', 'w') as fwrite_urls:
-            a = hf.get_files_in_dir_in_testData('.jpg', 'testData/testJPGs')
-            for item in a:
-                fwrite_urls.write("%s\n" % item)
-        '''
-        with open('./media/testData/testJPGs/jpeg_urls.txt', 'r') as fin:
-            raw_lines = fin.readlines()
-        lines = list(map(lambda s: s.strip(), raw_lines))
-        print(lines)
-        for i in range(len(lines)):
-            file_name = "test"+str(int(i+1))+".jpg"
-            print(file_name)
-            image = File(open("./media/testData/testJPGs/"+file_name, 'rb'))
+        for i in range(5):
+            image=File(open(a[i], 'rb'))
             new_photo = Photo(picture=image, caption="test", alt_text="TEST")
             new_photo.save()
-        '''
-        image = File(open("./media/testData/testJPGs/test1.jpg", 'rb'))
+            image.close()
+
+        
+        image = File(open(a[0], 'rb'))
         new_photo = Photo(picture=image, caption="test", alt_text="TEST")
         new_photo.save()
+        image.close()
+        image = File(open("./media/testData/testJPGs/test2.jpeg", 'rb'))
+        new_photo = Photo(picture=image, caption="test", alt_text="TEST")
+        new_photo.save()
+        image.close()
         '''
     def test_images_not_empty(self):
         self.assertNotEqual(Photo.objects.all().count(), 0, 'no photos!')
