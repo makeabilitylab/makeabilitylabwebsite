e84666b56fb1dab9993626827656200eba913354
Johnson Kuang <johnsonkuang@outlook.com>
2018-06-28 13:52:14 -0700

    for Shiven

--- website/test_cases/test_person.py
+++ website/test_cases/test_person.py
@@ -1,9 +1,27 @@
 from django.test import TestCase
+from website.models import Person, Position
 
-class PersonModelTests(TestCase):
+
+class PersonStarWarsTest(TestCase):
     def setUp(self):
-        pass
+        jkuang = Person.objects.create(first_name="Johnson", last_name="Kuang")
+        sbhatt = Person.objects.create(first_name="Shiven", last_name="Bhatt", )
+        jkuang.save()
+        sbhatt.save()
+
+    def test_StarWars_picture_exists(self):
+        p1 = Person.objects.all().get(first_name="Johnson")
+        p2 = Person.objects.all().get(first_name="Shiven")
+        self.assertNotEqual(p1, None, "The random star wars image retrieval is not working")
+        self.assertNotEqual(p2, None, "The random star wars image retrieval is not working")
 
 
-    def test_
+class PersonPositionTests(TestCase):
+    def setUp(self):
+        jkuang = Person.objects.create(first_name="Johnson", last_name="Kuang")
+        jkuang.save()
+        pos = Position
+
+    # def test_position_status(self):
+
 
