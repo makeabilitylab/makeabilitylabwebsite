3807ff9737bd015adfcf776b7713ccc9ea3f80c1
Shiven Bhatt <shivenbhatt@outlook.com>
2018-06-28 14:15:42 -0700

    for Kuang

--- website/test_cases/test_images.py
+++ website/test_cases/test_images.py
@@ -8,7 +8,7 @@ from website.test_cases.helper_functions import *
 
 class ImageModelTest(TestCase):
     def setUp(self):
-        operationGetThosePATHS = get_files_in_dir_in_media('.jpeg', 'testData/testJPGs')s
+        operationGetThosePATHS = get_files_in_dir_in_media('.jpeg', 'testData/testJPGs')
         for path in operationGetThosePATHS:
             image = File(open(path, 'rb'))
             new_photo = Photo.objects.create(picture=image, caption="test", alt_text="TEST")
