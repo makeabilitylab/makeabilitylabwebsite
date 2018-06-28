a039acc5faff76f0bc7252a25f3a80948ae5fdfc
Johnson Kuang <johnsonkuang@outlook.com>
2018-06-28 13:02:38 -0700

    working image ut's

--- website/test_cases/test_images.py
+++ website/test_cases/test_images.py
@@ -1,8 +1,7 @@
 from django.test import TestCase
 from website.models import Photo
-from django.core.files.images import ImageFile
 from django.core.files import File
-from PIL import Image
+
 
 import website.test_cases.helper_functions as hf
 
@@ -12,34 +11,16 @@ class ImageModelTest(TestCase):
         operationGetThoseURLS = hf.get_files_in_dir_in_testData('.jpeg', 'testData/testJPGs')
         for urls in operationGetThoseURLS:
             image = File(open(urls, 'rb'))
-            print(urls)
-            new_photo = Photo(picture=image, caption="test", alt_text="TEST")
-            print(new_photo)
-            new_photo.save()
-            image.close()
-        '''
-        for i in range(5):
-            image=File(open(a[i], 'rb'))
-            new_photo = Photo(picture=image, caption="test", alt_text="TEST")
+            new_photo = Photo.objects.create(picture=image, caption="test", alt_text="TEST")
             new_photo.save()
             image.close()
 
-        
-        image = File(open(a[0], 'rb'))
-        new_photo = Photo(picture=image, caption="test", alt_text="TEST")
-        new_photo.save()
-        image.close()
-        image = File(open("./media/testData/testJPGs/test2.jpeg", 'rb'))
-        new_photo = Photo(picture=image, caption="test", alt_text="TEST")
-        new_photo.save()
-        image.close()
-        '''
     def test_images_not_empty(self):
         self.assertNotEqual(Photo.objects.all().count(), 0, 'no photos!')
 
     def test_images_exist(self):
-        img = Photo.objects.all()[:1].get()
-        self.assertIsNotNone(img.picture, 'photo for ' + img.alt_text + ' is not there!')
+        for img in Photo.objects.all():
+            self.assertIsNotNone(img.picture, 'photo for ' + img.alt_text + ' is not there!')
 
     def test_image_url_exist(self):
         for img in Photo.objects.all():
