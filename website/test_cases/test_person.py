13f857b6346d3a7cdaeebebb7da7736c7bf5231e
Johnson Kuang <johnsonkuang@outlook.com>
2018-06-28 14:51:23 -0700

    person ut's done

--- website/test_cases/test_person.py
+++ website/test_cases/test_person.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from website.models import Person, Position
-from datetime import datetime
+from django.utils import timezone
+
+import datetime
 
 
 class PersonStarWarsTest(TestCase):
@@ -25,15 +27,19 @@ class PersonPositionTests(TestCase):
     def setUp(self):
         johns = Person.objects.create(first_name="Johnson", last_name="Kuang")
         johns.save()
-        pos = Position.objects.create(person=johns, start_date= datetime.now()+ datetime.timedelta(days=30),
-                                      end_date = datetime.now(), role="High School Student",title="Prof")
+        pos = Position.objects.create(person=johns, start_date=timezone.now() + datetime.timedelta(days=30),
+                                      end_date=timezone.now(), role="High School Student", title="Prof",
+                                      department="computer science information")
         pos.save()
 
     def test_position_exists(self):
         johns = Person.objects.all().get(first_name="Johnson")
         self.assertNotEqual(johns.get_latest_position(), None, "Position is not defined")
 
-    #def test_method_test(self):
+    def test_method_test(self):
+        johns = Person.objects.all().get(first_name="Johnson")
+        johns_pos = Position.objects.all().get(person=johns)
+        self.assertEqual(johns_pos.get_department_abbreviated(), "CS")
 
     # def test_position_status(self):
 
