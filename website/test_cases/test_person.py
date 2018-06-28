9f24371f4d04990bb3572f97c26bb1cdd26eb10c
Johnson Kuang <johnsonkuang@outlook.com>
2018-06-28 14:22:25 -0700

    Added person model ut's

--- website/test_cases/test_person.py
+++ website/test_cases/test_person.py
@@ -1,26 +1,39 @@
 from django.test import TestCase
 from website.models import Person, Position
+from datetime import datetime
 
 
 class PersonStarWarsTest(TestCase):
     def setUp(self):
-        jkuang = Person.objects.create(first_name="Johnson", last_name="Kuang")
-        sbhatt = Person.objects.create(first_name="Shiven", last_name="Bhatt", )
-        jkuang.save()
-        sbhatt.save()
+        johns = Person.objects.create(first_name="Johnson", last_name="Kuang")
+        shiv = Person.objects.create(first_name="Shiven", last_name="Bhatt", )
+        johns.save()
+        shiv.save()
 
     def test_StarWars_picture_exists(self):
-        p1 = Person.objects.all().get(first_name="Johnson")
-        p2 = Person.objects.all().get(first_name="Shiven")
-        self.assertNotEqual(p1, None, "The random star wars image retrieval is not working")
-        self.assertNotEqual(p2, None, "The random star wars image retrieval is not working")
+        johns = Person.objects.all().get(first_name="Johnson")
+        shiv = Person.objects.all().get(first_name="Shiven")
+        self.assertNotEqual(johns, None, "The random star wars image retrieval is not working")
+        self.assertNotEqual(shiv, None, "The random star wars image retrieval is not working")
+
+    def test_get_full_name(self):
+        johns = Person.objects.all().get(first_name="Johnson")
+        self.assertEqual(johns.get_full_name(), "Johnson Kuang")
 
 
 class PersonPositionTests(TestCase):
     def setUp(self):
-        jkuang = Person.objects.create(first_name="Johnson", last_name="Kuang")
-        jkuang.save()
-        pos = Position
+        johns = Person.objects.create(first_name="Johnson", last_name="Kuang")
+        johns.save()
+        pos = Position.objects.create(person=johns, start_date= datetime.now()+ datetime.timedelta(days=30),
+                                      end_date = datetime.now(), role="High School Student",title="Prof")
+        pos.save()
+
+    def test_position_exists(self):
+        johns = Person.objects.all().get(first_name="Johnson")
+        self.assertNotEqual(johns.get_latest_position(), None, "Position is not defined")
+
+    #def test_method_test(self):
 
     # def test_position_status(self):
 
