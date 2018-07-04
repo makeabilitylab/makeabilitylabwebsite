67dce5422924e9b3f11c113d7c9b471a74c841d9
Shiven Bhatt <shivenbhatt@outlook.com>
2018-07-04 12:02:49 -0700

    added projects.js #315

--- website/static/website/js/projects.js
+++ website/static/website/js/projects.js
@@ -1,5 +1,53 @@
 
-
+var keywords = [];
+var grid;
 $(window).load(function () {
-    console.log("doing stuff");
+    $grid = $('.grid').isotope({});
+
+    $('body').on('click', function (e) {
+        handleClick(e);
+    });
+
+    $('#keywords ul li').each(function(){
+        keywords.push($(this).text());
+    });
+
+    console.log(keywords);
+
 });
+
+
+function handleClick(e)
+{
+    if(isKeyword(e))
+    {
+        var filter = getFilter($(e.target).text());
+        console.log(filter);
+        $grid.isotope({ filter: filter});
+    }
+    else
+    {
+        $grid.isotope({ filter: '*'});
+    }
+}
+
+
+function getFilter(keyword)
+{
+    return "." + keyword.replace(" ", "-").replace("/", "-");
+}
+
+function isKeyword(e)
+{
+    if($(e.target) && $(e.target).text())
+    {
+        for (var i = 0; i < keywords.length; i++)
+        {
+            if($(e.target).text() === keywords[i])
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
