23bc755efde4b5604c8c9d8c8b4d91739eabe5a9
Johnson Kuang <johnsonkuang@outlook.com>
2018-08-03 11:17:37 -0700

    remove some previous changes

--- website/serializers.py
+++ website/serializers.py
@@ -11,51 +11,11 @@ class PublicationSerializer(serializers.ModelSerializer):
         #one to one:video
         #fk: talk
         #choice fields: pub_venue_type, award
-        fields = ('title',
-                  'authors',
-                  'pdf_file',
-                  'book_title',
-                  'book_title_short',
-                  'date',
-                  'num_pages',
-                  'projects',
-                  'project_umbrellas',
-                  'keywords',
-                  'page_num_start',
-                  'page_num_end',
-                  'official_url',
-                  'geo_location',
-                  'video',
-                  'talk',
-                  'series',
-                  'isbn',
-                  'doi',
-                  'publisher',
-                  'publisher_address',
-                  'acmid',
-                  'pub_venue_type',
-                  'extended_abstract',
-                  'peer_reviewed',
-                  'total_papers_submitted',
-                  'total_papers_accepted',
-                  'award')
+        fields = '__all__'
 
 class TalkSerializer(serializers.ModelSerializer):
     class Meta:
         model = Talk
         #Many to many fields: projects, project_umbralla, keywords, speakers
-        fields = ('title',
-                  'projects',
-                  'project_umbrellas',
-                  'keywords',
-                  'forum_name',
-                  'forum_url',
-                  'location',
-                  'speakers',
-                  'date',
-                  'slideshare_url',
-                  'pdf_file',
-                  'raw_file',
-                  'thumbnail'
-        )
+        fields = '__all__'
         depth = 10
