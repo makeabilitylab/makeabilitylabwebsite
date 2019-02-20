fbdaa70ca0053119e9a89751ff882f1dbfdfcab7
Hank Tadeusiak <hankt@uw.edu>
2019-02-20 15:46:20 -0800

    saving progress

--- website/static/website/js/lightbox.js
+++ website/static/website/js/lightbox.js
@@ -1,28 +1,64 @@
 "use strict";
 (function(){
+    let slideIndex = 1;
 
     window.onload = function(){
         let galleryImages = document.querySelectorAll(".project-gallery-col");
+        let lightboxImages = document.querySelectorAll(".myslides");
+        console.log(slideIndex)
         for (let i = 0; i < galleryImages.length; i++) {
             galleryImages[i].onclick = function() {
-                openLightBox();
+                slideIndex = galleryImages[i].id.split(/([0-9]+)/)[1];//find a better solution
+                console.log(slideIndex)
+                openLightBox(galleryImages[i].id, lightboxImages);
             };
         }
-        document.querySelector(".js-close-btn").onclick = closeLightBox;
 
+        document.querySelector(".prev").onclick = function() {
+            plusSlides(-1, lightboxImages);
+        };
+
+        document.querySelector(".next").onclick = function() {
+            plusSlides(1, lightboxImages);
+        };
+
+
+        document.querySelector(".js-lightbox-close-btn").onclick = function() {
+            closeLightBox(lightboxImages);
+        };
     };
 
     /**
      * C
      */
-    function openLightBox() {
-        alert("open");
+    function openLightBox(slide, lightboxImages) {
+        for (let i = 0; i < lightboxImages.length; i++) {
+            if (lightboxImages[i].id !== "lightbox-" + slide) {
+                lightboxImages[i].classList.add("is-hidden");
+            } else {
+                lightboxImages[i].classList.remove("is-hidden");
+            }
+        }
+        document.querySelector(".lightbox").classList.remove("is-hidden");
     }
 
     /**
      * E
      */
-    function closeLightBox() {
-        alert("close")
+    function closeLightBox(lightboxImages) {
+        document.querySelector(".lightbox").classList.add("is-hidden");
+        for (let i = 0; i < lightboxImages.length; i++) {
+            lightboxImages[i].classList.remove("is-hidden");
+        }
+    }
+
+    function plusSlides(n, lightboxImages) {
+        slideIndex = Number(n) + Number(slideIndex);// find better solution
+        if (slideIndex > lightboxImages.length) {
+            slideIndex = 1;
+        } else if (slideIndex < 1) {
+            slideIndex = lightboxImages.length;
+        }
+        openLightBox("slide" + slideIndex, lightboxImages);
     }
 })();
