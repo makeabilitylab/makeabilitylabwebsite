79ca5731aa17ee9adf9d05a6372ee4ae65d2e251
Hank Tadeusiak <hankt@uw.edu>
2019-02-23 15:38:23 -0800

    added comments for js file

--- website/static/website/js/lightbox.js
+++ website/static/website/js/lightbox.js
@@ -1,15 +1,20 @@
+//  Handles the basic functions of the lightbox for image gallery. User can click on an image in the image gallery
+//  to view it in a higher resolution. Once the lightbox is opened, the user change between the other photos in the
+//  gallery by clicking the arrows. Bellow each image in the lightbox is its assigned caption.
+
 "use strict";
 (function(){
-    let slideIndex;
-    let lightboxImages;
+    let slideIndex; //Keeps track of the current slide number
+    let lightboxImages; //Array of all the slides for the lightbox
 
     window.onload = function(){
         let galleryImages = document.querySelectorAll(".project-gallery-col");
         lightboxImages = document.querySelectorAll(".lightbox-slides");
         slideIndex = 1;
+
         for (let i = 0; i < galleryImages.length; i++) {
             galleryImages[i].onclick = function() {
-                slideIndex = galleryImages[i].id.split(/([0-9]+)/)[1];//find a better solution
+                slideIndex = galleryImages[i].id.split(/([0-9]+)/)[1];
                 openLightBox(galleryImages[i].id);
             };
         }
@@ -22,11 +27,12 @@
             slideChange(1);
         };
 
-        document.querySelector(".js-lightbox-close-btn").onclick = closeLightBox;
+        document.querySelector(".lightbox-close-btn").onclick = closeLightBox;
     };
 
     /**
-     * C
+     * Opens the lightbox to the image clicked on
+     * @param {object} slide - id of the slide containing the image clicked on
      */
     function openLightBox(slide) {
         for (let i = 0; i < lightboxImages.length; i++) {
@@ -40,7 +46,7 @@
     }
 
     /**
-     * E
+     * Closes the lightbox
      */
     function closeLightBox() {
         document.querySelector(".lightbox").classList.add("is-hidden");
@@ -50,10 +56,11 @@
     }
 
     /**
-     * E
+     * Changes the slide to the next or previous slide
+     * @param {int} slideNumber - Either '1' indicating the next slide or '-1' for the previous slide
      */
     function slideChange(slideNumber) {
-        slideIndex = Number(slideNumber) + Number(slideIndex);// find better solution
+        slideIndex = Number(slideNumber) + Number(slideIndex);
         if (slideIndex > lightboxImages.length) {
             slideIndex = 1;
         } else if (slideIndex < 1) {
