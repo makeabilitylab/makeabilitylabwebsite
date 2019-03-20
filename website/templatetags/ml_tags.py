a4b570e353ca5d56ccfc9b98a336021caf1a95d1
Hank Tadeusiak <hankt@uw.edu>
2019-03-20 14:17:44 -0700

    adds a custom template tag that removes any html from a string, removes the html from the news text previews

--- website/templatetags/ml_tags.py
+++ website/templatetags/ml_tags.py
@@ -1,6 +1,7 @@
 # Read more about custom tags and filters here: https://docs.djangoproject.com/en/dev/howto/custom-template-tags/#writing-custom-template-filters
 from django import template
 from django.template.defaultfilters import stringfilter
+import re
 
 from django.template.defaulttags import register
 
@@ -24,3 +25,11 @@ def jsdate(d):
         return "new Date({0},{1},{2})".format(d.year, d.month - 1, d.day)
     except AttributeError:
         return 'undefined'
+
+ # Removes any HTML tags in String and returns new String
+@register.filter(name='removehtmltags')
+@stringfilter
+def removehtmltags(value):
+    cleanr = re.compile('<.*?>')
+    cleantext = re.sub(cleanr, '', value)
+    return cleantext
