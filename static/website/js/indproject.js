c1849ba5b1e6b323f47d303dfa9ceab47e45e489
Johnson Kuang <johnsonkuang@outlook.com>
2019-08-28 12:33:25 -0700

    removed static files

--- static/website/js/indproject.js
+++
@@ -1,9 +0,0 @@
-$(window).load(function(){
-	heightFix('.people-col');
-    heightFix('.video-column');
-});
-
-$(window).resize(function() {
-	heightFix('.people-col');
-    heightFix('.video-column');
-});
