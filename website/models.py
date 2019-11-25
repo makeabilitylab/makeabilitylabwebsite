f2ba144dfd0d8e5e3e700a41b64f85833c208432
Jon Froehlich <jonfroehlich@gmail.com>
2019-11-25 15:07:46 -0800

    Attempts to address #679
    There was some really hacky code in here that I fixed. I think we should, in general, try not to have our models generate html. Instead, we can use Python data directly using templates in Django

--- website/models.py
+++ website/models.py
@@ -884,6 +884,8 @@ class Project_Role(models.Model):
 
 
 # This class contains the image or video which will appear in the top description of each project. It functions as a combination of Photo and Video, but is separated to make it simpler to have a specific video or photo as the projects header.
+# TODO: I want to rename Project_header to ProjectHeader
+# but it looks like this is a bit of a pain, see: https://stackoverflow.com/questions/25091130/django-migration-strategy-for-renaming-a-model-and-relationship-fields
 class Project_header(models.Model):
     title = models.CharField(max_length=255)
     title.help_text = "These fields are used as the image in the about section. To add a banner to your page go to the banners table and assign banners to your project using the project field there. This field will accept both a video and an image. If both are provided the video will be used."
@@ -892,17 +894,13 @@ class Project_header(models.Model):
     image = models.ImageField(upload_to='projects/images/', blank=True, null=True, max_length=255)
     project = models.ForeignKey(Project, blank=True, null=True, on_delete=models.CASCADE)
 
-    def get_visual(self):
-        if self.video_url:
-            url = self.get_embed()
-            return '<iframe class=\"video-about\" src=\"' + url + '\" frameborder="0" allowfullscreen></iframe>'
-        elif self.image:
-            return "<img class=\"video-about\" src=\"" + self.image.url + "\">"
+    def has_video(self):
+        """Returns true if the video_url is set"""
+        return self.video_url is not None
 
-    def get_embed(self):
-        base_url = "https://youtube.com/embed"
-        unique_url = self.video_url[self.video_url.find("/", 9):]
-        return base_url + unique_url
+    def get_video_embed(self):
+        """Returns video embed code"""
+        return get_video_embed(self.video_url)
 
     class Meta:
         verbose_name = "Project About Visual"
@@ -950,18 +948,7 @@ class Video(models.Model):
 
     def get_embed(self):
         """Given self.video_url, returns the correctly formatted video embed url for YouTube and Vimeo"""
-        if 'youtu.be' in self.video_url or 'youtube.com' in self.video_url:
-            # https://youtu.be/i0IDbHGir-8 or https://www.youtube.com/watch?v=i0IDbHGir-8
-
-            base_url = "https://youtube.com/embed"
-            unique_url = self.video_url[self.video_url.find("/", 9):]
-
-            # See https://developers.google.com/youtube/youtube_player_demo for details on parameterizing YouTube video
-            return base_url + unique_url + "?showinfo=0&iv_load_policy=3"
-        elif 'vimeo' in self.video_url:
-            # https://player.vimeo.com/video/164630179
-            vimeo_video_id = self.video_url.rsplit('/', 1)[-1]
-            return "https://player.vimeo.com/video/" + vimeo_video_id
+        return get_video_embed(self.video_url)
 
     # Returns a cap case title
     def get_title(self):
@@ -983,6 +970,25 @@ class Video(models.Model):
 def repl_func(m):
     """process regular expression match groups for word upper-casing problem"""
     return m.group(1) + m.group(2).upper()
+
+
+def get_video_embed(video_url):
+    """Returns proper embed code for a video url"""
+
+    if 'youtu.be' in video_url or 'youtube.com' in video_url:
+        # https://youtu.be/i0IDbHGir-8 or https://www.youtube.com/watch?v=i0IDbHGir-8
+
+        base_url = "https://youtube.com/embed"
+        unique_url = video_url[video_url.find("/", 9):]
+
+        # See https://developers.google.com/youtube/youtube_player_demo for details on parameterizing YouTube video
+        return base_url + unique_url + "?showinfo=0&iv_load_policy=3"
+    elif 'vimeo' in video_url:
+        # https://player.vimeo.com/video/164630179
+        vimeo_video_id = video_url.rsplit('/', 1)[-1]
+        return "https://player.vimeo.com/video/" + vimeo_video_id
+    else:
+        return "unknown video service"
 
 
 class Talk(models.Model):
