991fceaf48008aaeaa423c9fbb55a67d479609af
Jon Froehlich <jonfroehlich@gmail.com>
2019-12-11 06:14:24 -0800

    removed commented code for auto-entry from bibtex
    this feature would have been nice when first building the site but it's no longer needed (and we never got it working perfectly)

--- website/admin.py
+++ website/admin.py
@@ -250,69 +250,6 @@ class PublicationAdmin(admin.ModelAdmin):
             kwargs["widget"] = widgets.FilteredSelectMultiple("keywords", is_stacked=False)
         return super(PublicationAdmin, self).formfield_for_manytomany(db_field, request, **kwargs)
 
-    # Uncomment this function to enable auto-entry from bibtex
-    # The following code is based in part on a hint by this Stackoverflow post: http://stackoverflow.com/a/4952370
-    # See: http://stackoverflow.com/a/10041463 for overiding admin forms
-    # def add_view(self, request, **kwargs):
-
-    #     if request.method == 'POST':
-    #         # Stage 1 form submitted, parse data and redirect to with data in url to get Django to auto-fill in form
-    #         extra_context = {}
-
-    #         extra_context['test_var'] = 'In stage 2!'
-
-    #         bibtex_str = request.POST['bibtex_textarea']
-    #         if 'submitbibtex_button' in request.POST and len(bibtex_str) > 0:
-
-    #             bib_database = bibtexparser.loads(bibtex_str)
-    #             bib_entry = bib_database.entries[0]
-    #             # str_items = ""
-    #             # for key in bib_entry:
-    #             #     str_items = str_items + "{}={}&".format(key, bib_entry[key])
-    #             #
-    #             # raw_url = '/admin/website/publication/add/?{}'.format(str_items)
-    #             # prepared_url = urllib.parse.urlencode(raw_url)
-    #             #return redirect()
-
-    #             # TODO: The problem is that passing values in the url only works if those values are already existing
-    #             # django entries. So, for example, if you pass in a bibtex that has a new, never-before-seen author
-    #             # then we need to create that Author database entry first. I also cannot actually get one-to-many fields
-    #             # like authors to actually pass correctly by value in the url.
-    #             params = urllib.parse.urlencode(bib_entry)
-    #             redirect_url = '/admin/website/publication/add/?{}'.format(params)
-    #             return redirect(redirect_url)
-    #             #return redirect('/admin/website/publication/add/?title="test"')
-    #         else:
-    #             return redirect('/admin/website/publication/add/?title=')
-
-    #     elif not request.GET:
-
-    #         opts = self.model._meta
-    #         app_label = opts.app_label
-
-    #         template = loader.get_template('admin/website/publication/bibtex_form.html')
-    #         context = RequestContext(request)
-
-    #         # because bibtex_form.thml extends the 'admin/change_form.html' template, we need to add in some variables
-    #         # that change_form.html requires. See: http://stackoverflow.com/questions/28777376/problems-extend-change-form-html-in-django-admin
-    #         # and specifically the answer: http://stackoverflow.com/a/28777461
-    #         context.push(
-    #                 {"test_var": "in stage 1",
-    #                  'opts': opts,
-    #                  'app_label': app_label,
-    #                  'change': False,
-    #                  'is_popup': False,
-    #                  'save_as': False,
-    #                  'has_delete_permission': False,
-    #                  'has_add_permission': False,
-    #                  'has_change_permission': False}
-    #         )
-    #         return HttpResponse(template.render(context))
-    #     elif request.GET:
-    #         extra_context = {}
-    #         extra_context['test_var'] = 'In stage 1'
-    #         return super(PublicationAdmin, self).add_view(request, extra_context=extra_context, **kwargs)
-
 admin.site.register(Person, PersonAdmin)
 admin.site.register(Publication, PublicationAdmin)
 admin.site.register(Talk, TalkAdmin)
