33ffc1a2043f2cef479c05e558bc88f3b6691c15
jonfroehlich <jonfroehlich@gmail.com>
2023-09-28 14:37:33 -0700

    fixed else condition

--- makeabilitylab/settings.py
+++ makeabilitylab/settings.py
@@ -33,7 +33,7 @@ if "DJANGO_ENV" in os.environ:
        config_file = os.path.join(BASE_DIR, 'config.ini')
        config.read(config_file)
        CONFIG_FILE = config_file
-else
+else:
     CONFIG_FILE = "No config file set"
 
 # Quick-start development settings - unsuitable for production
