061c133fa34a93b15d44e0f88c33891413bc7a6b
jonfroehlich <jonfroehlich@gmail.com>
2023-09-30 06:57:09 -0700

    Oops, I had formatted this file wrong. Fixed now

--- website/management/commands/print_environment_vars.py
+++ website/management/commands/print_environment_vars.py
@@ -1,6 +1,12 @@
+from django.core.management.base import BaseCommand, CommandError
 import os
 
-print("Environmental variables:")
+class Command(BaseCommand):
 
-for k, v in os.environ.items():
-    print(f'\t{k}={v}')
+    help='This command prints out all the environmental variables visible to Django. Useful for debugging.'
+
+    def handle(self, *args, **options):
+        print("Environmental variables:")
+
+        for k, v in os.environ.items():
+            print(f'\t{k}={v}')
