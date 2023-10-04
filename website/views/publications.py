d2c254567877398b658b1c27d7f3cd03e62472e1
jonfroehlich <jonfroehlich@gmail.com>
2023-10-03 17:53:22 -0700

    setting up new pubs page using snippets

--- website/views/publications.py
+++ website/views/publications.py
@@ -25,8 +25,17 @@ def publications(request):
     # See https://stackoverflow.com/a/4668703
     # sampledate__gte=datetime.date(2011, 1, 1),
     # Old: Publication.objects.filter(date__range=["2012-01-01", date.today()]),
-    publications = Publication.objects.filter(date__gte=settings.DATE_MAKEABILITYLAB_FORMED)
+    publications = Publication.objects.filter(date__gte=settings.DATE_MAKEABILITYLAB_FORMED).order_by('-date')
+
+    # Loop through pubs and store in a dict with year : list of pubs ordered by date
+    map_year_to_pub_list = dict()
+    for pub in publications:
+        if pub.date.year not in map_year_to_pub_list:
+            map_year_to_pub_list[pub.date.year] = list()
+        map_year_to_pub_list[pub.date.year].append(pub)
+
     context = {'publications': publications,
+               'map_year_to_pub_list': map_year_to_pub_list,
                'banners': displayed_banners,
                'filter': filter,
                'groupby': groupby,
