f192c31a7449232ed367fab5b85316f34dbb508d
jonfroehlich <jonfroehlich@gmail.com>
2023-10-06 17:25:53 -0700

    trying to support adaptive navbar; updated filenames to make them more relevant; updated dynamic people.css layout using media queries

--- website/static/website/js/top-navbar.js
+++ website/static/website/js/top-navbar.js
@@ -1,4 +1,6 @@
 /*!
+ * Based on Grayscale:
+ * 
  * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
  * Code licensed under the Apache License v2.0.
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
