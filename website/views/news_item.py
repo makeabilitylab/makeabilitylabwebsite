e8fa9c86450c3ae9ee805a6a80e9a218dbd0f20f
jonfroehlich <jonfroehlich@gmail.com>
2023-10-19 08:40:18 -0700

    working on the news sidebar

--- website/views/news_item.py
+++ website/views/news_item.py
@@ -13,6 +13,10 @@ import logging
 # This retrieves a Python logging instance (or creates it)
 _logger = logging.getLogger(__name__)
 
+MAX_RECENT_MAKEABILITY_LAB_NEWS = 3
+MAX_RECENT_NEWS_ITEMS_FOR_PROJECT = 3
+MAX_RECENT_NEWS_ITEMS_BY_AUTHOR = 3
+
 def news_item(request, slug=None, id=None):
     func_start_time = time.perf_counter()
 
@@ -28,28 +32,29 @@ def news_item(request, slug=None, id=None):
     # news = get_object_or_404(News, slug=slug)
     # news = get_object_or_404(News, pk=news_id)
 
-    max_extra_items = 4  # Maximum number of authors
-    all_author_news = news.author.news_set.order_by('-date')
-    author_news = []
+    recent_ml_news = News.objects.order_by('-date')[:MAX_RECENT_MAKEABILITY_LAB_NEWS]
+
+    # max_extra_items = 4  # Maximum number of authors
+    # all_author_news = news.author.news_set.order_by('-date')
+    # author_news = []
     
-    for item in all_author_news:
-        if item != news:
-            author_news.append(item)
+    # for item in all_author_news:
+    #     if item != news:
+    #         author_news.append(item)
 
-    project_news = {}
+    # project_news = {}
 
-    if news.project != None:
-        for project in news.project.all():
-            ind_proj_news = []
-            all_proj_news = project.news_set.order_by('-date')
-            for item in all_proj_news:
-                if item != news:
-                    ind_proj_news.append(item)
-            project_news[project] = ind_proj_news[:max_extra_items]
-
-    context = {'news': news,
-               'author_news': author_news[:max_extra_items],
-               'project_news': project_news,
+    # if news.project != None:
+    #     for project in news.project.all():
+    #         ind_proj_news = []
+    #         all_proj_news = project.news_set.order_by('-date')
+    #         for item in all_proj_news:
+    #             if item != news:
+    #                 ind_proj_news.append(item)
+    #         project_news[project] = ind_proj_news[:max_extra_items]
+    print(recent_ml_news);
+    context = {'news_item': news,
+               'recent_ml_news': recent_ml_news,
                'navbar_white': True,
                'debug': settings.DEBUG}
     
