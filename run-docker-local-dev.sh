46980d20c3578827d9c77493decc8897c8245228
jonfroehlich <jonfroehlich@gmail.com>
2023-10-18 17:03:30 -0700

    updated local dev script to build/start docker image

--- run-docker-local-dev.sh
+++ run-docker-local-dev.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-# Run docker-compose command
+if [ "$1" == "--help" ]; then
+  echo "Usage: $0 [OPTION]..."
+  echo "Run local development environment in Docker."
+  echo ""
+  echo "Options:"
+  echo "--build     Build the Docker image before starting the services."
+  echo "--help      Display this help and exit."
+  exit
+fi
+
+if [ "$1" == "--build" ]
+then
+    docker build . -t makelab_image
+fi
+
 docker-compose -f docker-compose-local-dev.yml up
