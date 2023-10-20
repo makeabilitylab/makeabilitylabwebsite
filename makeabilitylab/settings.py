b8ddf53d3112c2f3ef4a1453743512b59bf6f285
jonfroehlich <jonfroehlich@gmail.com>
2023-10-20 15:33:26 -0700

    updated to 1.6.3 with redesigned news item page

--- makeabilitylab/settings.py
+++ makeabilitylab/settings.py
@@ -72,7 +72,7 @@ else:
     ALLOWED_HOSTS = ['*']
 
 # Makeability Lab Global Variables
-ML_WEBSITE_VERSION = "1.6.1 (added django-sortedm2m-filter-horizontal-widget back in; reduced requirements.txt; removed slug edit field from NewsAdmin)"
+ML_WEBSITE_VERSION = "1.6.3 (redesigned news item page)"
 DATE_MAKEABILITYLAB_FORMED = datetime.date(2012, 1, 1)  # Date Makeability Lab was formed
 MAX_BANNERS = 7 # Maximum number of banners on a page
 
