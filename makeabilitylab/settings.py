3cf4b0b0eca4bb7868e363f31290c1e239fdcc21
Jon Froehlich <jonf@cs.uw.edu>
2023-10-26 16:43:52 -0700

    updated to 1.6.4: redesigned banner handling

--- makeabilitylab/settings.py
+++ makeabilitylab/settings.py
@@ -72,7 +72,7 @@ else:
     ALLOWED_HOSTS = ['*']
 
 # Makeability Lab Global Variables
-ML_WEBSITE_VERSION = "1.6.3 (redesigned news item page)"
+ML_WEBSITE_VERSION = "1.6.4 (redesigned banner handling)"
 DATE_MAKEABILITYLAB_FORMED = datetime.date(2012, 1, 1)  # Date Makeability Lab was formed
 MAX_BANNERS = 7 # Maximum number of banners on a page
 
