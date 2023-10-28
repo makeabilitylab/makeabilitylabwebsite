37c321533900d126d62235887e793452b2356d96
jonfroehlich <jonfroehlich@gmail.com>
2023-10-28 06:09:25 -0700

    updated image column in banner admin interface; fixes #1086

--- website/admin/banner_admin.py
+++ website/admin/banner_admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from website.models import Banner
+from django.utils.html import format_html
 from image_cropping import ImageCroppingMixin
 
 @admin.register(Banner)
@@ -9,10 +10,9 @@ class BannerAdmin(ImageCroppingMixin, admin.ModelAdmin):
     # - fields, a list of fields you want to display in order
     # - fieldsets, allows you to organize fields into sets
     fieldsets = [
-        
-        ('Banner Image', {'fields': ["image", "alt_text", "cropping"]}),
+        ('Banner Title and Caption', {'fields': ["title", "caption", "link"]}),
         ('Banner Video', {'fields': ["video"]}),
-        ('Banner Title and Caption', {'fields': ["title", "caption"]}),
+        ('Banner Image', {'fields': ["image", "alt_text", "cropping"]}),
         ('Banner Pages', {'fields': ["landing_page", "project"]}),
         ('Banner Properties', {'fields': ["favorite", "date_added"]})
         # ('Image', {'fields': ["image", "image_preview"]})
@@ -21,9 +21,16 @@ class BannerAdmin(ImageCroppingMixin, admin.ModelAdmin):
 
     # The list display lets us control what is shown in the default persons table at Home > Website > Banners
     # info on displaying multiple entries comes from http://stackoverflow.com/questions/9164610/custom-columns-using-django-admin
-    list_display = ('title', 'project', 'landing_page', 'favorite', 'image')
+    list_display = ('title', 'project', 'landing_page', 'favorite', 'get_media_url')
 
     autocomplete_fields = ['project']
     readonly_fields = ('date_added',)
 
     # readonly_fields = ["image_preview"]
+
+    def get_media_url(self, obj):
+        """Either returns the video url or the image url, if specified"""
+        media_url = obj.image.url if obj.image else obj.video.url if obj.video else None
+        return format_html('<a href="{}">{}</a>', media_url, media_url) if media_url else None
+        
+    get_media_url.short_description = 'Media'
