80609ac0d8adfb3ca59edbeb1dc1a1d88dc1b556
Jon Froehlich <jonf@cs.uw.edu>
2023-11-09 15:57:44 -0800

    Replaced slow python based query/filter logic with ORM
    Towards https://github.com/makeabilitylab/makeabilitylabwebsite/issues/1065, I replaced the slow python based query/filter logic with Django ORM--Django's Object-Relational Mapper (ORM), which enables you to interact with your database

--- website/admin/news_admin.py
+++ website/admin/news_admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from website.models import News, Person
+from website.models import News, Person, Position
 from image_cropping import ImageCroppingMixin
 from django.contrib.admin import widgets
+
+from django.db.models import Q
+from datetime import date
 
 @admin.register(News)
 class NewsAdmin(ImageCroppingMixin, admin.ModelAdmin):
@@ -12,23 +15,39 @@ class NewsAdmin(ImageCroppingMixin, admin.ModelAdmin):
     exclude = ('slug',)
 
     def display_projects(self, obj):
+        """Displays the projects linked to the news story"""
         return ", ".join([project.name for project in obj.project.all()])
     
     display_projects.short_description = 'Projects'
 
     def display_people(self, obj):
+        """Displays the people linked to the news story"""
         return ", ".join([person.get_full_name() for person in obj.people.all()])
     display_people.short_description = 'People'
 
     # Filters authors only to current members and sorts by firstname
     # Based on: http://stackoverflow.com/a/30627555
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
+
         # print("NewsAdmin.formfield_for_foreignkey: db_field: {} db_field.name {} request: {}".format(db_field, db_field.name, request))
         if db_field.name == "author":
-            current_member_ids = [person.id for person in Person.objects.all() if person.is_current_member]
-            filtered_persons = Person.objects.filter(id__in=current_member_ids).order_by('first_name')
-            #print("filtered_persons", filtered_persons)
-            kwargs["queryset"] = filtered_persons
+            # Define the conditions for a current member. Current members need to have:
+            # 1. Have started today or in the past
+            # 2. Have an end date of null or in the future
+            # 3. Be a current member 
+            current_member_conditions = (Q(start_date__lte=date.today()) &
+                                         (Q(end_date__isnull=True) | Q(end_date__gte=date.today())) &
+                                         Q(role=Position.MEMBER)) # must be a member of the lab
+
+            # Get all current members
+            current_member_positions = Position.objects.filter(current_member_conditions)
+
+            # Get the related Person objects and sort by first name
+            current_members = Person.objects.filter(
+                id__in=current_members.values_list('person', flat=True)).order_by('first_name')
+            
+            kwargs["queryset"] = current_members
+        
         return super(NewsAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
     def formfield_for_manytomany(self, db_field, request=None, **kwargs):
