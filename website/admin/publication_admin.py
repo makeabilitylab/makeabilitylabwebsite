957d4d25292fa2bc3eb900d84afb63d50fb7c7f8
jonfroehlich <jonfroehlich@gmail.com>
2023-11-10 11:51:34 -0800

    I removed autocomplete support for now because I spent hours and hours trying to debug #1093 and I couldn't figure it out

--- website/admin/publication_admin.py
+++ website/admin/publication_admin.py
@@ -25,12 +25,20 @@ class PublicationAdmin(admin.ModelAdmin):
 
     list_filter = (PubVenueTypeListFilter, PubVenueListFilter)
 
-    # add in auto-complete fields for talks, see:
-    #   https://docs.djangoproject.com/en/3.0/ref/contrib/admin/#django.contrib.admin.ModelAdmin.autocomplete_fields
-    # this addresses: https://github.com/jonfroehlich/makeabilitylabwebsite/issues/553
+    # add in auto-complete fields 
+    #   this addresses: https://github.com/jonfroehlich/makeabilitylabwebsite/issues/553
+    #
+    # autocomplete_fields is a list of ForeignKey and/or ManyToManyField fields you would like 
+    # to change to Select2 autocomplete inputs. By default, the admin uses a select-box interface (<select>) for those fields.
+    # The Select2 input looks similar to the default input but comes with a search feature that loads the options asynchronously. 
+    # This is faster and more user-friendly if the related model has many instances.
+    #
     # You must also update the search_fields in the respective admins like PosterAdmin, VideoAdmin, and TalkAdmin
     # these search fields become what the auto-complete function searches for filtering
-    autocomplete_fields = ['poster', 'video', 'talk']
+    # See : https://docs.djangoproject.com/en/4.2/ref/contrib/admin/#django.contrib.admin.ModelAdmin.autocomplete_fields
+    # Update Nov 10, 2023: This is now broken due to weirdness with Select2 fields in the admin interface
+    # See: https://github.com/makeabilitylab/makeabilitylabwebsite/issues/1093
+    #autocomplete_fields = ['poster', 'video', 'talk']
 
     def get_form(self, request, obj=None, **kwargs):
         """We custom style some of the admin UI, including expanding the width of the talk select interface"""
@@ -45,15 +53,27 @@ class PublicationAdmin(admin.ModelAdmin):
         #   https://stackoverflow.com/q/35211809
         # to address: https://github.com/jonfroehlich/makeabilitylabwebsite/issues/851
         text_min_width = 750
-        form.base_fields['title'].widget.attrs['style'] = 'min-width: {}px;'.format(text_min_width)
-        form.base_fields['book_title'].widget.attrs['style'] = 'min-width: {}px;'.format(text_min_width)
-        form.base_fields['book_title_short'].widget.attrs['style'] = 'min-width: {}px;'.format(500)
+        form.base_fields['title'].widget.attrs['style'] = f'min-width: {text_min_width}px;'
+        form.base_fields['book_title'].widget.attrs['style'] = f'min-width: {text_min_width}px;'
+        form.base_fields['book_title_short'].widget.attrs['style'] = f'min-width: 500px;'
 
         select_min_width = 600
-        form.base_fields['poster'].widget.attrs['style'] = 'min-width: {}px;'.format(select_min_width)
-        form.base_fields['video'].widget.attrs['style'] = 'min-width: {}px;'.format(select_min_width)
-        form.base_fields['talk'].widget.attrs['style'] = 'min-width: {}px;'.format(select_min_width)
+        select_max_width = 800
+        custom_style = f'min-width: {select_min_width}px; max-width: {select_max_width}px;'
+        form.base_fields['poster'].widget.attrs['style'] = custom_style
+        form.base_fields['video'].widget.attrs['style'] = custom_style
+        form.base_fields['talk'].widget.attrs['style'] = custom_style
         return form
+    
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+
+        # In this code, we’re checking if the db_field is one of ‘video’, ‘talk’, or ‘poster’. 
+        # If it is, we’re ordering the queryset for that field by ‘date’ in descending order (hence the ‘-date’). 
+        if db_field.name in ['video', 'talk', 'poster']:
+            kwargs["queryset"] = db_field.related_model.objects.order_by('-date')
+
+        # If the db_field is not one of those fields, we’re just calling the parent class’s formfield_for_foreignkey method.
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
     def formfield_for_manytomany(self, db_field, request=None, **kwargs):
         if db_field.name == "authors":
