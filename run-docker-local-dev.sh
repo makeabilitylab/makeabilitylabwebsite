ea20d1288c567858171b6b7b2edea6378f353f57
jonfroehlich <jonfroehlich@gmail.com>
2023-11-10 11:17:37 -0800

    added --no-cache option

--- run-docker-local-dev.sh
+++ run-docker-local-dev.sh
@@ -6,13 +6,15 @@ if [ "$1" == "--help" ]; then
   echo ""
   echo "Options:"
   echo "--build     Build the Docker image before starting the services."
+  echo "--buildnc   Build the Docker image without cache before starting the services."
   echo "--help      Display this help and exit."
   exit
 fi
 
-if [ "$1" == "--build" ]
-then
+if [ "$1" == "--build" ]; then
     docker build . -t makelab_image
+elif [ "$1" == "--buildnc" ]; then
+    docker build --no-cache . -t makelab_image
 fi
 
 docker-compose -f docker-compose-local-dev.yml up
