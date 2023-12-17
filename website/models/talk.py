b63fbaefa6db2a0aaff81cd8f698e77f888f285e
Jon Froehlich <jonf@cs.uw.edu>
2023-12-17 05:19:06 -0800

    updated some comments

--- website/models/talk.py
+++ website/models/talk.py
@@ -6,6 +6,7 @@ from django.db import models
 # IntegerChoices and TextChoices. These extend Python’s Enum types with extra constraints 
 # and functionality to make them suitable for Field.choices1. 
 class TalkType(models.TextChoices):
+    """The talk type choices"""
     INVITED_TALK = "Invited Talk"
     CONFERENCE_TALK = "Conference Talk"
     MS_DEFENSE = "MS Defense"
@@ -15,6 +16,10 @@ class TalkType(models.TextChoices):
     KEYNOTE_TALK = "Keynote Talk"
 
 class Talk(Artifact):
+    """
+    The Talk class inherits from the Artifact class and represents a talk or presentation.
+    It includes fields for the talk type, slideshare URL, video, and upload directories.
+    """
     UPLOAD_DIR = 'talks/'
     THUMBNAIL_DIR = os.path.join(UPLOAD_DIR, 'images/')
 
@@ -29,33 +34,15 @@ class Talk(Artifact):
     talk_type.help_text = "If this is a conference talk (e.g., for CHI, ASSETS, UIST, IMWUT), please select 'Conference Talk'"
 
     def get_upload_dir(self, filename):
+        """Gets the upload directory for this artifact. This is required by the parent class."""
         return os.path.join(self.UPLOAD_DIR, filename)
 
     def get_upload_thumbnail_dir(self, filename):
+        """Gets the upload thumbnail directory for this artifact. This is required by the parent class."""
         return os.path.join(self.THUMBNAIL_DIR, filename)
     
-    def get_person(self):
-        """Gets the "first author" (or speaker in this case) for the talk"""
-        return self.authors.all()[0]
-    
-    def get_first_speaker_last_name(self):
-        speakers = self.authors.all()
-        if speakers.exists():
-            return speakers.first().last_name
-        else:
-            return None
-
     def get_speakers_as_csv(self):
         """Gets the list of speakers as a csv string"""
-        # iterate through all of the speakers and return the csv
-        is_first_speaker = True
-        list_of_speakers_as_csv = ""
-        for speaker in self.authors.all():
-            if is_first_speaker != True:
-                # if not the first speaker, add in a comma in CSV string
-                list_of_speakers_as_csv += ", "
-            list_of_speakers_as_csv += speaker.get_full_name()
-            is_first_speaker = False
-        return list_of_speakers_as_csv
+        return ', '.join(speaker.get_full_name() for speaker in self.authors.all())
 
     get_speakers_as_csv.short_description = 'Speaker List'
