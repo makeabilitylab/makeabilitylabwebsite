edf2eb0c04021efcc0eca7f2445c4fbd3214c1d4
Jon Froehlich <jonf@cs.uw.edu>
2023-12-21 11:28:31 -0800

    Added grant id and some file naming updates

--- website/admin/grant_admin.py
+++ website/admin/grant_admin.py
@@ -18,14 +18,27 @@ class GrantAdmin(ArtifactAdmin):
     search_fields = ['title', 'forum_name']
 
     fieldsets = [
-        (None,                      {'fields': ['title', 'authors', 'date', 'end_date', 'sponsor']}),
-        ('Files',                   {'fields': ['pdf_file', 'raw_file']}),
+        (None,                      {'fields': ['title', 'authors']}),
+        ('Grant Info',              {'fields': ['date', 'end_date', 'sponsor', 'funding_amount', 'forum_url', 'grant_id']}),
+        ('Grant Files',             {'fields': ['pdf_file', 'raw_file']}),
         ('Project Info',            {'fields': ['projects', 'project_umbrellas']}),
         ('Keyword Info',            {'fields': ['keywords']}),
     ]
 
     def get_form(self, request, obj=None, **kwargs):
         form = super().get_form(request, obj, **kwargs)
+
+        form.base_fields['authors'].label = 'PIs and Co-PIs'
+        form.base_fields['authors'].help_text = "The first author is assumed to be the PI. Co-PIs should be listed in the order they appear on the grant."
+
         form.base_fields['date'].label = 'Start date'
         form.base_fields['date'].help_text = 'Start date for the grant'
+
+        form.base_fields['forum_url'].label = 'Grant url'
+        grant_url = "https://www.nsf.gov/awardsearch/showAward?AWD_ID=1302338"
+        form.base_fields['forum_url'].help_text = f'The grant url (e.g., <a href="{grant_url}">{grant_url}</a>)'
+
+        form.base_fields['pdf_file'].label = 'Grant PDF'
+        form.base_fields['raw_file'].help_text = 'The raw file (e.g., Word Docx, Overleaf Zip, etc.) for <b>archival</b> purposes. This is not shown on the website.'
+
         return form
--- website/models/grant.py
+++ website/models/grant.py
@@ -2,6 +2,14 @@ import os
 from django.db import models
 from website.models import Artifact
 
+from django.db.models.signals import pre_save # to handle when sponsors change
+from django.dispatch import receiver # to handle when sponsors change
+
+import logging # for logging
+
+# This retrieves a Python logging instance (or creates it)
+_logger = logging.getLogger(__name__)
+
 class Grant(Artifact):
     UPLOAD_DIR = 'grants/'
     THUMBNAIL_DIR = os.path.join(UPLOAD_DIR, 'images/')
@@ -11,18 +19,55 @@ class Grant(Artifact):
     sponsor = models.ForeignKey('Sponsor', on_delete=models.PROTECT)
     sponsor.help_text = "Sponsor of the grant"
 
-    end_date = models.DateField(null=True)
+    # In Django, to make a field optional in the admin form, you need to set blank=True in addition 
+    # to null=True in your model field. The null=True allows the database to store a NULL value 
+    # for the field, while blank=True allows the field to be blank in forms, including the admin form.
+    end_date = models.DateField(blank=True, null=True)
     end_date.help_text = "End date for this grant"
 
     funding_amount = models.IntegerField(null=True)
     funding_amount.help_text = "Amount of funding (in USD) for this grant"
 
+    grant_id = models.CharField(max_length=255, null=True)
+    grant_id.help_text = "The grant id (e.g., <a href='https://www.nsf.gov/awardsearch/showAward?AWD_ID=1302338'>1302338</a>)"
+
     @property
     def start_date(self):
         return self.date
+    
+    @property
+    def grant_url(self):
+        return self.forum_url
+    
+    @grant_url.setter
+    def grant_url(self, value):
+        self.forum_url = value
 
     def get_upload_dir(self, filename):
         return os.path.join(self.UPLOAD_DIR, filename)
 
     def get_upload_thumbnail_dir(self, filename):
         return os.path.join(self.THUMBNAIL_DIR, filename)
+    
+    def save(self, *args, **kwargs):
+        if self.forum_name is None:
+            _logger.warning(f"Grant {self.title} has no forum_name, setting to sponsor")
+
+            if self.sponsor.short_name is not None:
+                self.forum_name = self.sponsor.short_name
+            elif self.sponsor.name is not None:
+                self.forum_name = self.sponsor.name
+
+        super(Grant, self).save(*args, **kwargs)
+    
+@receiver(pre_save, sender=Grant)
+def check_sponsor_change(sender, instance, **kwargs):
+    if instance.id is not None:  # this is not a new object
+        old_grant = Grant.objects.get(id=instance.id)
+        if old_grant.sponsor != instance.sponsor:
+            _logger.warning(f"Grant {instance.title} changed sponsor from {old_grant.sponsor} to {instance.sponsor}.")
+
+            if instance.sponsor.short_name is not None:
+                instance.forum_name = instance.sponsor.short_name
+            elif instance.sponsor.name is not None:
+                instance.forum_name = instance.sponsor.name
