f00b6e7ead8c787e336f777b51d8c1a01607dfe3
jonfroehlich <jonfroehlich@gmail.com>
2023-12-22 09:08:55 -0800

    minor sponsor updates

--- makeabilitylab/settings.py
+++ makeabilitylab/settings.py
@@ -72,7 +72,7 @@ else:
     ALLOWED_HOSTS = ['*']
 
 # Makeability Lab Global Variables
-ML_WEBSITE_VERSION = "1.7.4.1 (fixed some minor sponsor stuff)"
+ML_WEBSITE_VERSION = "1.7.4.2 (minor sponsor updates)"
 DATE_MAKEABILITYLAB_FORMED = datetime.date(2012, 1, 1)  # Date Makeability Lab was formed
 MAX_BANNERS = 7 # Maximum number of banners on a page
 
--- website/admin/sponsor_admin.py
+++ website/admin/sponsor_admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
 from website.models import Sponsor
 from image_cropping import ImageCroppingMixin
+from django.db.models import Q, Sum
+from django.utils import timezone
 
 @admin.register(Sponsor)
 class SponsorAdmin(ImageCroppingMixin, admin.ModelAdmin):
-    pass
+    # The list display lets us control what is shown in the default talk table at Home > Website > Sponsors
+    # See: https://docs.djangoproject.com/en/dev/ref/contrib/admin/#django.contrib.admin.ModelAdmin.list_display
+    list_display = ('name', 'short_name', 'total_funding', 'grant_count', 'active_grant_count')
+
+    def total_funding(self, obj):
+        return obj.grant_set.aggregate(total_funding=Sum('funding_amount'))['total_funding']
+    total_funding.short_description = 'Total Funding'
+
+    def grant_count(self, obj):
+        return obj.grant_set.count()
+    grant_count.short_description = 'Grants'
+
+    def active_grant_count(self, obj):
+        return obj.grant_set.filter(Q(end_date__isnull=True) | Q(end_date__gt=timezone.now())).count()
+    active_grant_count.short_description = 'Active Grants'
+
