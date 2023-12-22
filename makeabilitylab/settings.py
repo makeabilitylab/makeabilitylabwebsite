0d615eedf1cef843511faef2bf899953033ccdbf
jonfroehlich <jonfroehlich@gmail.com>
2023-12-22 06:17:38 -0800

    add sponsors to landing page

--- makeabilitylab/settings.py
+++ makeabilitylab/settings.py
@@ -72,7 +72,7 @@ else:
     ALLOWED_HOSTS = ['*']
 
 # Makeability Lab Global Variables
-ML_WEBSITE_VERSION = "1.7.3 (added grant tracking functionality)"
+ML_WEBSITE_VERSION = "1.7.4 (added sponsors to landing page)"
 DATE_MAKEABILITYLAB_FORMED = datetime.date(2012, 1, 1)  # Date Makeability Lab was formed
 MAX_BANNERS = 7 # Maximum number of banners on a page
 
