6662ac22b5159d5b9a73abddd6e22b7f94748718
jonfroehlich <jonfroehlich@gmail.com>
2023-12-22 05:20:44 -0800

    upped version to 1.7.3

--- makeabilitylab/settings.py
+++ makeabilitylab/settings.py
@@ -72,7 +72,7 @@ else:
     ALLOWED_HOSTS = ['*']
 
 # Makeability Lab Global Variables
-ML_WEBSITE_VERSION = "1.7.2 (fixed publication page rendering slowness)"
+ML_WEBSITE_VERSION = "1.7.3 (added grant tracking functionality)"
 DATE_MAKEABILITYLAB_FORMED = datetime.date(2012, 1, 1)  # Date Makeability Lab was formed
 MAX_BANNERS = 7 # Maximum number of banners on a page
 
