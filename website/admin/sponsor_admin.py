6dfdca76dc4246e0af2813d062f4c91b489ac002
Jon Froehlich <jonf@cs.uw.edu>
2024-01-01 15:50:06 -0800

    added sponsor logo to admin list

--- website/admin/sponsor_admin.py
+++ website/admin/sponsor_admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
 from website.models import Sponsor
+from website.models.sponsor import SPONSOR_THUMBNAIL_SIZE
 from image_cropping import ImageCroppingMixin
 from django.db.models import Q, Sum
 from django.utils import timezone
+
+from django.utils.html import format_html # for formatting thumbnails
+from easy_thumbnails.files import get_thumbnailer # for generating thumbnails
+import os # for checking if thumbnail file exists
 
 @admin.register(Sponsor)
 class SponsorAdmin(ImageCroppingMixin, admin.ModelAdmin):
     # The list display lets us control what is shown in the default talk table at Home > Website > Sponsors
     # See: https://docs.djangoproject.com/en/dev/ref/contrib/admin/#django.contrib.admin.ModelAdmin.list_display
-    list_display = ('name', 'short_name', 'total_funding', 'grant_count', 'active_grant_count')
+    list_display = ('name', 'get_display_thumbnail', 'short_name', 'total_funding', 'grant_count', 'active_grant_count')
 
     def total_funding(self, obj):
         return obj.grant_set.aggregate(total_funding=Sum('funding_amount'))['total_funding']
@@ -21,4 +26,16 @@ class SponsorAdmin(ImageCroppingMixin, admin.ModelAdmin):
     def active_grant_count(self, obj):
         return obj.grant_set.filter(Q(end_date__isnull=True) | Q(end_date__gt=timezone.now())).count()
     active_grant_count.short_description = 'Active Grants'
+
+    def get_display_thumbnail(self, obj):
+        if obj.icon and os.path.isfile(obj.icon.path):
+            # Use easy_thumbnails to generate a thumbnail
+            thumbnailer = get_thumbnailer(obj.icon)
+            thumbnail_options = {'size': (SPONSOR_THUMBNAIL_SIZE[0], SPONSOR_THUMBNAIL_SIZE[1]), 'crop': True}
+            thumbnail_url = thumbnailer.get_thumbnail(thumbnail_options).url
+
+            return format_html('<img src="{}" height="50" style="border-radius: 5%;"/>', thumbnail_url)
+        return 'No Thumbnail'
+    
+    get_display_thumbnail.short_description = 'Logo'
 
