130c159cd4431247f63c05ff9c13717adb92ce51
Jon Froehlich <jonf@cs.uw.edu>
2024-02-06 17:06:26 -0800

    Updated to 1.9.3.2

--- makeabilitylab/settings.py
+++ makeabilitylab/settings.py
@@ -72,7 +72,7 @@ else:
     ALLOWED_HOSTS = ['*']
 
 # Makeability Lab Global Variables
-ML_WEBSITE_VERSION = "1.9.3 (updates project pages with data, related projects, etc.)"
+ML_WEBSITE_VERSION = "1.9.3.2 (slight tweaks to project pages)"
 DATE_MAKEABILITYLAB_FORMED = datetime.date(2012, 1, 1)  # Date Makeability Lab was formed
 MAX_BANNERS = 7 # Maximum number of banners on a page
 
