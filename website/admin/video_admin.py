a55c4234b80764dc1f7b2825dcfb1e9f9ff9a2cc
Jon Froehlich <jonf@cs.uw.edu>
2024-02-07 15:01:29 -0800

    updated widget to select multiple projects for videos

--- website/admin/video_admin.py
+++ website/admin/video_admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from website.models import Video
+from django.contrib.admin import widgets
 
 @admin.register(Video)
 class VideoAdmin(admin.ModelAdmin):
@@ -18,3 +19,22 @@ class VideoAdmin(admin.ModelAdmin):
         return ", ".join([project.name for project in obj.projects.all()])
     
     display_projects.short_description = 'Projects'
+
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        """
+        Overrides the formfield_for_manytomany method of the parent ModelAdmin class to customize the widgets 
+        used for ManyToMany fields in the admin interface.
+
+        Parameters:
+        db_field (Field): The database field being processed.
+        request (HttpRequest): The current Django HttpRequest object capturing all details of the request.
+        **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+        formfield (FormField): The formfield to be used in the admin interface for the ManyToMany field. The 
+        widget of the formfield is customized based on the name of the db_field.
+        """
+        if db_field.name == "projects":
+            kwargs["widget"] = widgets.FilteredSelectMultiple("projects", is_stacked=False)
+
+        return super(VideoAdmin, self).formfield_for_manytomany(db_field, request, **kwargs) 
