42326c33dce41d73984ca4437d71ee78b5e55c1a
Jon Froehlich <jonf@cs.uw.edu>
2024-09-19 10:59:36 -0700

    updagted to version 1.9.4: fixes forum name
    https://github.com/makeabilitylab/makeabilitylabwebsite/issues/1166

--- makeabilitylab/settings.py
+++ makeabilitylab/settings.py
@@ -72,7 +72,7 @@ else:
     ALLOWED_HOSTS = ['*']
 
 # Makeability Lab Global Variables
-ML_WEBSITE_VERSION = "1.9.3.5 (add social media og:share for project pages, better mobile support)"
+ML_WEBSITE_VERSION = "1.9.4 (fixes forum name)"
 DATE_MAKEABILITYLAB_FORMED = datetime.date(2012, 1, 1)  # Date Makeability Lab was formed
 MAX_BANNERS = 7 # Maximum number of banners on a page
 
