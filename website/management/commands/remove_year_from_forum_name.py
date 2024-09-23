221ac5b2f9ef907c6c0bb8b4a89f3f78dda39fe5
Jon Froehlich <jonf@cs.uw.edu>
2024-09-23 05:41:03 -0700

    added more debug output

--- website/management/commands/remove_year_from_forum_name.py
+++ website/management/commands/remove_year_from_forum_name.py
@@ -10,12 +10,13 @@ import logging
 _logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
-    help = "This is a one time use command to update poster filenames"
+    help = "This is a one time use command to update forum names for publications"
 
     def handle(self, *args, **options):
         _logger.debug("Running remove_year_from_forum_name.py to remove the year from forum names. Should only be run once")
         
         # Loop through all publications and clean up the forum name
+        changed_forum_names = []
         for pub in Publication.objects.all():
             old_forum_name = pub.forum_name
             new_forum_name = ml_utils.clean_forum_name(old_forum_name)  
@@ -25,3 +26,11 @@ class Command(BaseCommand):
                 pub.save(update_fields=['forum_name'])
 
                 _logger.debug(f"The forum name for {pub} has been renamed from {old_forum_name} to {pub.forum_name}")
+                changed_forum_names.append((old_forum_name, pub))
+        
+        # Print out stats about the changes
+        _logger.debug(f"Checked {Publication.objects.all().count()} publications, changed {len(changed_forum_names)} forum names.")
+        for old_name, pub in changed_forum_names:
+            _logger.debug(f"{pub.title}: changed {old_name} to {pub.forum_name}")
+
+            
