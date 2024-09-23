48ab45edbb616f0069c10b51dccf3539aec184b9
Jon Froehlich <jonf@cs.uw.edu>
2024-09-23 15:03:15 -0700

    updated version to 1.9.4.7

--- makeabilitylab/settings.py
+++ makeabilitylab/settings.py
@@ -72,7 +72,7 @@ else:
     ALLOWED_HOSTS = ['*']
 
 # Makeability Lab Global Variables
-ML_WEBSITE_VERSION = "1.9.4.6 (fourth try to address auto-bio bug #1181)"
+ML_WEBSITE_VERSION = "1.9.4.7 (added fuzzy matching for pub pdfs #1173)"
 DATE_MAKEABILITYLAB_FORMED = datetime.date(2012, 1, 1)  # Date Makeability Lab was formed
 MAX_BANNERS = 7 # Maximum number of banners on a page
 
