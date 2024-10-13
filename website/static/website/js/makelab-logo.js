1dcac3d9a72ce428ad4f51b066344a9cf52f803c
Jon Froehlich <jonf@cs.uw.edu>
2024-10-13 09:34:42 -0700

    updated makelab animation

--- website/static/website/js/makelab-logo.js
+++ website/static/website/js/makelab-logo.js
@@ -9,7 +9,8 @@
  * @module makelab-logo
  */
 
-import { MakeabilityLabLogoExploder, MakeabilityLabLogo} from 'https://cdn.jsdelivr.net/gh/makeabilitylab/js@main/dist/makelab.logo.js';
+//import { MakeabilityLabLogoExploder, MakeabilityLabLogo} from 'https://cdn.jsdelivr.net/gh/makeabilitylab/js@main/dist/makelab.logo.js';
+import { MakeabilityLabLogoExploder, MakeabilityLabLogo} from './makelab.all.js';
 
 // Set up the logo animation
 let canvas = document.getElementById('makelab-logo-canvas');
@@ -18,7 +19,8 @@ canvas.height = 300;
 let triangleSize = 70;
 let ctx = canvas.getContext('2d');
 
-let bgFillColor = "white";
+let bgFillColor = "rgba(255, 255, 255, 0.2)";
+//let bgFillColor = "rgba(255, 0, 0, 1)";
 let startFillColor = "rgba(255, 255, 255, 0.5)";
 let xPos = canvas.width/2 - MakeabilityLabLogo.getWidth(triangleSize) / 2;
 let makeLabLogoExploder = new MakeabilityLabLogoExploder(xPos, 10, triangleSize, startFillColor);
@@ -35,7 +37,7 @@ window.addEventListener('scroll', scrollHandler, { passive: true });
   */
 function scrollHandler() {
   const scrollY = window.scrollY;
-  const lerpAmt = Math.min(scrollY / 100, 1); // Adjust the 100 as needed
+  const lerpAmt = Math.min(scrollY / 150, 1); // Adjust the 100 as needed
 
   makeLabLogoExploder.update(lerpAmt);
   draw(ctx);
@@ -59,22 +61,50 @@ const parentDiv = document.querySelector('.col-md-6.center-canvas');
 
 const resizeObserver = new ResizeObserver(entries => {
   
-  const newWidth = entries[0].contentRect.width;
-  console.log("Parent div width changed! ", newWidth);
+  const parentDivRect = entries[0].contentRect;
+  console.log("Parent div dimensions changed! ", parentDivRect);
 
   const boundingClientRect = entries[0].target.getBoundingClientRect();
   console.log("Bounding client rect: ", boundingClientRect);
-  if (newWidth < 500) {
-    // Handle the case where parent div is less than 500px wide
-    console.log(`Parent div is less than 500px wide! It's ${newWidth}px wide.`);
-    // You can potentially adjust the animation here
-    //makeLabLogoExploder.reset(newWidth, canvas.height);
-    const logoSize = Math.max(newWidth * 0.9, 200);
-    console.log("New logo size:", logoSize);
-    makeLabLogoExploder.setLogoSize(logoSize);
-    //makeLabLogoExploder.centerLogo(boundingClientRect.width, boundingClientRect.height);
-    draw(ctx);
-  }
+  // if (newWidth < 500) {
+  //   // Handle the case where parent div is less than 500px wide
+  //   console.log(`Parent div is less than 500px wide! It's ${newWidth}px wide.`);
+  //   // You can potentially adjust the animation here
+  //   //makeLabLogoExploder.reset(newWidth, canvas.height);
+  //   const logoSize = Math.max(newWidth * 0.9, 200);
+  //   console.log("New logo size:", logoSize);
+  //   makeLabLogoExploder.setLogoSize(logoSize);
+  //   canvas.width = newWidth;
+  //   //makeLabLogoExploder.centerLogo(boundingClientRect.width, boundingClientRect.height);
+  //   makeLabLogoExploder.centerLogo(newWidth, boundingClientRect.height);
+  //   draw(ctx);
+  // }
+
+  
+  // Handle the case where parent div is less than 500px wide
+  console.log(`Parent div is ${parentDivRect.width}px wide.`);
+  // You can potentially adjust the animation here
+  //makeLabLogoExploder.reset(newWidth, canvas.height);
+  const maxLogoWidth = Math.min(parentDivRect.width, canvas.width);
+  const maxLogoHeight = Math.min(parentDivRect.height, canvas.height);
+  const maxLogoSize = Math.max(maxLogoWidth, maxLogoHeight);
+  console.log(`maxLogoWidth: ${maxLogoWidth}, maxLogoHeight: ${maxLogoHeight}, maxLogoSize: ${maxLogoSize}`);
+  const logoSize = maxLogoSize * 1;
+  console.log("New logo size:", logoSize);
+  //makeLabLogoExploder.setLogoSize(logoSize);
+  makeLabLogoExploder.fitToCanvas(maxLogoWidth, maxLogoHeight);
+  //makeLabLogoExploder.setLogoSize(500);
+  canvas.width = parentDivRect.width;
+  canvas.height = parentDivRect.height; //Math.max(300, parentDivRect.height);
+  //makeLabLogoExploder.centerLogo(boundingClientRect.width, boundingClientRect.height);
+  //makeLabLogoExploder.centerLogo(newWidth, boundingClientRect.height);
+  //makeLabLogoExploder.setLogoPosition(0, 0);
+  makeLabLogoExploder.centerLogo(parentDivRect.width, canvas.height);
+  
+  //TODO: we want to center the logo across x but not y
+
+  draw(ctx);
+  
 });
 resizeObserver.observe(parentDiv);
 
@@ -115,4 +145,38 @@ function draw(ctx){
   ctx.fillStyle = bgFillColor;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   makeLabLogoExploder.draw(ctx);
+
+  const parentDivDimensions = parentDiv.getBoundingClientRect();
+
+  // set text properties
+  ctx.fillStyle = 'black';
+  ctx.font = '16px Arial';
+
+  // show width and height of logo
+  const logoDimensionsText = `Logo: ${makeLabLogoExploder.finalWidth.toFixed(1)} x ${makeLabLogoExploder.finalHeight.toFixed(1)}`;
+  // console.log(logoDimensionsText);
+  ctx.textAlign = 'center';
+  const logoDimensionsTextMetrics = ctx.measureText(logoDimensionsText);
+  ctx.fillText(logoDimensionsText, canvas.width / 2, canvas.height - 20);
+
+  // measure and draw canvas.width centered at the top
+  const marginText = 4;
+  const widthText = "Canvas: " + canvas.width.toString() +
+    " Parent div: " + parentDivDimensions.width;
+  const widthTextMetrics = ctx.measureText(widthText);
+  ctx.textAlign = 'center';
+  ctx.textBaseline = 'top';
+  ctx.fillText(widthText, canvas.width / 2, marginText);
+
+  // measure and draw canvas.height rotated 90 degrees and centered vertically
+  const heightText = "Canvas: " + canvas.height.toString() +
+    " Parent div: " + parentDivDimensions.height.toFixed(1);
+  const heightTextMetrics = ctx.measureText(heightText);
+  ctx.save();
+  ctx.translate(0, canvas.height / 2);
+  ctx.rotate(-Math.PI / 2);
+  ctx.textAlign = 'center';
+  ctx.textBaseline = 'top';
+  ctx.fillText(heightText, 0, marginText);
+  ctx.restore();
 }
