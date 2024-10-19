49c8b774578b1dffb01985da1ba53cd9a110752f
Jon Froehlich <jonf@cs.uw.edu>
2024-10-19 06:31:22 -0700

    Adds auto-fill functionality to video_admin
    The new VideoAdmin form (change_form) can be auto-filled from either an associated talk or publication

--- website/admin/video_admin.py
+++ website/admin/video_admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from website.models import Video
+from website.models import Video, Publication, Talk
 from django.contrib.admin import widgets
+import logging
+
+# This retrieves a Python logging instance (or creates it)
+_logger = logging.getLogger(__name__)
 
 @admin.register(Video)
 class VideoAdmin(admin.ModelAdmin):
@@ -19,6 +23,40 @@ class VideoAdmin(admin.ModelAdmin):
         return ", ".join([project.name for project in obj.projects.all()])
     
     display_projects.short_description = 'Projects'
+
+    def get_changeform_initial_data(self, request):
+        # _logger.debug("******* get_changeform_initial_data ***********")
+        # _logger.debug(f"request is {request} and request.GET is {request.GET}")
+
+        initial = super().get_changeform_initial_data(request)
+        publication_id = request.GET.get('publication_id')
+        talk_id = request.GET.get('talk_id')
+        if publication_id:
+            _logger.debug(f"publication_id is {publication_id}")
+            try:
+                publication = Publication.objects.get(id=publication_id)
+                initial.update({
+                    'title': publication.title,
+                    'date': publication.date,
+                })
+                # For ManyToManyField, set the initial data as a list of IDs
+                initial['projects'] = publication.projects.all().values_list('id', flat=True)
+
+            except Publication.DoesNotExist:
+                pass
+        elif talk_id:
+            _logger.debug(f"talk_id is {talk_id}")
+            try:
+                talk = Talk.objects.get(id=talk_id)
+                initial.update({
+                    'title': talk.title,
+                    'date': talk.date,
+                })
+                # For ManyToManyField, set the initial data as a list of IDs
+                initial['projects'] = talk.projects.all().values_list('id', flat=True)
+            except Talk.DoesNotExist:
+                pass
+        return initial
 
     def formfield_for_manytomany(self, db_field, request, **kwargs):
         """
