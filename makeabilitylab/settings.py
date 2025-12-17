b00e87efd0bc5a3acfdacfb370a2706fd8f7985d
jonfroehlich <jonfroehlich@gmail.com>
2025-12-17 15:23:25 -0800

    Bumped version to 1.9.9.9

--- makeabilitylab/settings.py
+++ makeabilitylab/settings.py
@@ -72,8 +72,8 @@ else:
     ALLOWED_HOSTS = ['*']
 
 # Makeability Lab Global Variables, including Makeability Lab version
-ML_WEBSITE_VERSION = "1.9.9.8" # Keep this updated with each release and also change the short description below
-ML_WEBSITE_VERSION_DESCRIPTION = "Updated admin UI landing page"
+ML_WEBSITE_VERSION = "1.9.9.9" # Keep this updated with each release and also change the short description below
+ML_WEBSITE_VERSION_DESCRIPTION = "Fixes poster upload bugs"
 DATE_MAKEABILITYLAB_FORMED = datetime.date(2012, 1, 1)  # Date Makeability Lab was formed
 MAX_BANNERS = 7 # Maximum number of banners on a page
 
