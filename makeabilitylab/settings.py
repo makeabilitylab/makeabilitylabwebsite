cc11ed60b52b252b567bcd43d6e56ea04602bbd2
jonfroehlich <jonfroehlich@gmail.com>
2026-01-01 12:07:18 -0800

    slight update to about us section of makeability lab landing page

--- makeabilitylab/settings.py
+++ makeabilitylab/settings.py
@@ -72,8 +72,8 @@ else:
     ALLOWED_HOSTS = ['*']
 
 # Makeability Lab Global Variables, including Makeability Lab version
-ML_WEBSITE_VERSION = "2.2.4" # Keep this updated with each release and also change the short description below
-ML_WEBSITE_VERSION_DESCRIPTION = "Fixes talk cards on mobile"
+ML_WEBSITE_VERSION = "2.2.5" # Keep this updated with each release and also change the short description below
+ML_WEBSITE_VERSION_DESCRIPTION = "Slight update to index.html About Us section"
 DATE_MAKEABILITYLAB_FORMED = datetime.date(2012, 1, 1)  # Date Makeability Lab was formed
 MAX_BANNERS = 7 # Maximum number of banners on a page
 
