46d4ce4422fde6457cd99fbc91f71f21bba6ec84
jonfroehlich <jonfroehlich@gmail.com>
2026-02-12 21:31:27 -0800

    bumped version to 2.2.7 with addition of triangle art animation

--- makeabilitylab/settings.py
+++ makeabilitylab/settings.py
@@ -72,8 +72,8 @@ else:
     ALLOWED_HOSTS = ['*']
 
 # Makeability Lab Global Variables, including Makeability Lab version
-ML_WEBSITE_VERSION = "2.2.6" # Keep this updated with each release and also change the short description below
-ML_WEBSITE_VERSION_DESCRIPTION = "Updated Makeability Lab animation on landing page to include text"
+ML_WEBSITE_VERSION = "2.2.7" # Keep this updated with each release and also change the short description below
+ML_WEBSITE_VERSION_DESCRIPTION = "Added triangle art holiday easter egg to homepage"
 DATE_MAKEABILITYLAB_FORMED = datetime.date(2012, 1, 1)  # Date Makeability Lab was formed
 MAX_BANNERS = 7 # Maximum number of banners on a page
 
