99265fd64d8a01175392d441e86ceb2850a4c23b
jonfroehlich <jonfroehlich@gmail.com>
2026-04-04 14:13:20 -0700

    Added highlight published with the lab checkbox

--- makeabilitylab/settings.py
+++ makeabilitylab/settings.py
@@ -72,8 +72,8 @@ else:
     ALLOWED_HOSTS = ['*']
 
 # Makeability Lab Global Variables, including Makeability Lab version
-ML_WEBSITE_VERSION = "2.2.8c" # Keep this updated with each release and also change the short description below
-ML_WEBSITE_VERSION_DESCRIPTION = "Improved view-project-people page"
+ML_WEBSITE_VERSION = "2.2.8d" # Keep this updated with each release and also change the short description below
+ML_WEBSITE_VERSION_DESCRIPTION = "Improved view-project-people page (highlight published)"
 DATE_MAKEABILITYLAB_FORMED = datetime.date(2012, 1, 1)  # Date Makeability Lab was formed
 MAX_BANNERS = 7 # Maximum number of banners on a page
 
